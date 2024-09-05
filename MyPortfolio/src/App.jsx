ad13cc79de8742e34a8f9d81e3eab4b87530943a
booyaalex <you@example.com>
2024-09-05 10:06:11 -0400

    Yes

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -4,7 +4,7 @@ export function Home() {
   return (
     <>
       <header>
-        <div className="flex evenly">
+        <div className="flex around">
           <div>
             <div className="section-alt round">
               <img id="header-image" className="round" src="./src/assets/img/headerImage.jpg" alt="Header Image" title="Yes I have red contacts. No I'm not high in this picture."></img>
@@ -17,7 +17,7 @@ export function Home() {
                 <h2>Programmer and Web Developer</h2>
               </div>
               <div>
-                <p>I create simple interactive websites for people who want a clean and modern look. I excel at using html5 to create websites that look and feel great.</p>
+                <p className="split center">I create simple interactive websites for people who want a clean and modern look. I excel at using html5 to create websites that look and feel great.</p>
               </div>
             </div>
           </div>
@@ -37,7 +37,7 @@ export function Home() {
 export function About() {
   return (
     <>
-      <header className="title">
+      <header>
         <h1 className="center">About Me</h1>
       </header>
       <div className="flex nowrap">
@@ -53,21 +53,20 @@ export function About() {
           </div>
           <div className="section full flex between center round">
             <div className="flex split vertical-center center">
-              <i class="fa-regular fa-user"></i>
+              <i className="fa-regular fa-user"></i>
               <h2>Leadership</h2>
             </div>
             <div className="flex split center vertical-center">I am able to take control of a situation and lead others to success.</div>
           </div>
           <div className="section full flex between center round">
             <div className="flex split vertical-center center">
-              <i class="fa-solid fa-comment"></i>
+              <i className="fa-regular fa-comment"></i>
               <h2>Open Minded</h2>
             </div>
             <div className="flex split center vertical-center">I'm able to take criticism and feedback well, and I'm able to apply that feedback into my work.</div>
           </div>
         </div>
       </div>
-
     </>
   );
 }
@@ -75,17 +74,18 @@ export function About() {
 export function Projects() {
   return (
     <>
-      <header className="title">
+      <header>
         <h1 className="center">My Projects</h1>
         <h3 className="center">Here are my projects that I created.</h3>
       </header>
-      <div className="flex evenly">
-        <div className="project center section round">
-          <a href="https://blooket.games" target="_blank">
-            <img className="round" src="./src/assets/img/blooketLeaderboard.png" alt="Blooket Leaderboards"></img>
-          </a>
-          <h2>Blooket Leaderboards</h2>
-          <p>A website inspired by blooket that allows you to upload your blooket stats and comapre them with others.</p>
+      <div className="flex nowrap">
+        <img id="about-me-image" src="./src/assets/img/blooketLeaderboard.jpg" alt="About Me Image"></img>
+        <div className="section-alt split center">
+          <h1>Blooket Leaderboards</h1>
+          <br></br>
+          <h3>A website inspired by blooket that allows you to upload your blooket stats and comapre them with others. It ranks these players based on how well you do, and it updates automatically apon user upload. You can also try to break world records with video proof, giving more insentive to participate.</h3>
+          <br></br>
+          <a href="https://blooket.games" target="_blank">Check out the website here!</a>
         </div>
       </div>
     </>
@@ -106,26 +106,16 @@ export function Certification() {
         <h1 className="center">My Certificates</h1>
         <h3 className="center">Here are my Awards and Certificates that show off my abilities.</h3>
       </header>
-      <div className="certificate flex evenly">
-        <div>
-          <a href="./src/assets/img/schoolCertificate.jpg" target="_blank">
-            <img className="center" src="./src/assets/img/schoolCertificate.jpg" alt="School Certificate"></img>
-          </a>
+      <div className="flex around">
+        <div className="certificate center section round">
+          <img className="round" src="./src/assets/img/schoolCertificate.jpg" alt="School Certificate"></img>
+          <h1>Certificate of Achievement</h1>
+          <h3>Most Outstanding: Programming and Web Development</h3>
         </div>
-        <div>
-          <h3 className="center">Certificate of Achievement: Most Outstanding (Programming & Web)</h3>
-        </div>
-      </div>
-      <br></br>
-      <br></br>
-      <div className="certificate flex evenly">
-        <div>
-          <a href="./src/assets/img/certiportCertificate.png" target="_blank">
-            <img className="center" src="./src/assets/img/certiportCertificate.png" alt="School Certificate"></img>
-          </a>
-        </div>
-        <div>
-          <h3 className="center">HTML and CSS Certification</h3>
+        <div className="certificate center section round">
+          <img className="round" src="./src/assets/img/certiportCertificate.png" alt="Certiport Certificate"></img>
+          <h1>HTML and CSS Certification</h1>
+          <h3>Information Technology Specialist in HTML and CSS</h3>
         </div>
       </div>
     </>
@@ -134,6 +124,50 @@ export function Certification() {
 
 export function Contact() {
   return (
-    <></>
+    <>
+      <header>
+        <div className="flex evenly">
+          <div>
+            <div className="section-alt round">
+              <img id="header-image" className="round" src="./src/assets/img/contactHeaderImage.jpg" alt="Header Image"></img>
+            </div>
+          </div>
+          <div>
+            <div className="flex column around center">
+              <div>
+                <h1>Contact Me</h1>
+                <h2>I'm more than happy to hear from you!</h2>
+              </div>
+              <div>
+                <h3>You can reach me from:</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="section round center">
+        <div className="flex vertical-center between">
+          <div className="flex vertical-center">
+            <i className="fa-solid fa-phone"></i>
+            <h2>Phone</h2>
+          </div>
+          <a href="tel:5084167911">(508)-416-XXXX</a>
+        </div>
+        <div className="flex vertical-center between">
+          <div className="flex vertical-center">
+            <i className="fa-solid fa-envelope"></i>
+            <h2>Home Email</h2>
+          </div>
+          <a href="mailto:isidororossinia@gmail.com">isidororossinia@gmail.com</a>
+        </div>
+        <div className="flex vertical-center between">
+          <div className="flex vertical-center">
+            <i className="fa-solid fa-envelope"></i>
+            <h2>School Email</h2>
+          </div>
+          <a href="mailto:aisidororossini2027@jpkeefehs.org" target="_blank">aisidororossini2027@jpkeefehs.org</a>
+        </div>
+      </div>
+    </>
   );
 }
